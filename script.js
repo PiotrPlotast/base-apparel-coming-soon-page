8cc7eebd79b2f7c49e127a6995713074daf57f87
Pietrell <piotrplotast2006@gmail.com>
2023-10-30 21:55:49 +0100

    added js email validation

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+const form = document.querySelector('.hero__form');
 const emailInput = document.querySelector('.email');
 const submitButton = document.querySelector('.submit-btn');
+
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const email = emailInput.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // TODO - ad error icon and message
+    if (!emailRegex.test(email)) {
+        emailInput.style.borderColor = 'hsl(0, 93%, 68%)';
+        emailInput.style.color = 'hsl(0, 93%, 68%)';
+        // emailInput.style.backgroundImage = 'url(images/icon-error.svg) no-repeat 50% 50%';
+    } else {
+        emailInput.style.borderColor = '';
+        emailInput.style.color = '';
+        // emailInput.style.backgroundImage = '';
+    }
+});

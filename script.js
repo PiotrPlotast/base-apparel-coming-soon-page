999c2f78b73474d6fd5807ecabc94bce4ffa599e
Pietrell <piotrplotast2006@gmail.com>
2023-10-31 19:11:53 +0100

    finished writing script and styles

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const form = document.querySelector('.hero__form');
 const emailInput = document.querySelector('.email');
 const submitButton = document.querySelector('.submit-btn');
-
+const errorMessage = document.querySelector('.error');
 
 submitButton.addEventListener('click', function (event) {
     event.preventDefault();
     const email = emailInput.value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // TODO - ad error icon and message
+
     if (!emailRegex.test(email)) {
         emailInput.style.borderColor = 'hsl(0, 93%, 68%)';
         emailInput.style.borderWidth = '2px';
-        emailInput.style.color = 'hsl(0, 93%, 68%)';
         emailInput.style.backgroundImage = 'url(images/icon-error.svg)';
         emailInput.style.backgroundRepeat = 'no-repeat';
         emailInput.style.backgroundPosition = 'right 120px center';
+        errorMessage.style.display = 'block';
     } else {
         emailInput.style.borderColor = '';
-        emailInput.style.color = '';
         emailInput.style.backgroundImage = '';
+        errorMessage.style.display = 'none';
     }
 });
